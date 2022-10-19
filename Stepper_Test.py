083c05786d6bbab9927a068c7c62d31f2bb7a959
Lutz Hager <lutz.hager@online.de>
2022-10-19 18:39:47 +0200

    added test script for Stepper

--- Stepper_Test.py
+++ Stepper_Test.py
@@ -5,3 +5,16 @@
 #   Date: 19.10.22
 #
 #----------------------------------------------------------------------------------------------------------------
+#sudo pip3 install adafruit-circuitpython-motorkit
+# infos: https://learn.adafruit.com/adafruit-dc-and-stepper-motor-hat-for-raspberry-pi?view=all
+
+from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
+import time
+import board
+
+kit = MotorKit()
+
+for i in range(100):
+    kit.stepper1.onestep()
+    time.sleep(0.01)
