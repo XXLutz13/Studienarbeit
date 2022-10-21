2a481866b201ff59d8f5e062027ccda109291f87
Lutz Hager <lutz.hager@online.de>
2022-10-21 09:59:02 +0200

    added cobotta test script

---
+++ cobotta_Test.py
@@ -0,0 +1,11 @@
+#----------------------------------------------------------------------------------------------------------------
+#   Test script for cobotta python access
+#
+#   Author: Lutz Hager 
+#   Date: 21.10.22
+#
+#----------------------------------------------------------------------------------------------------------------
+import pybcapclient.bcapclient as bcapclient
+import numpy as np
+import cv2
+import time
