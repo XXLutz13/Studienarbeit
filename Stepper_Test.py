02b9e0c61ead015784e427365ceaa51e0e685e56
Lutz Hager <lutz.hager@online.de>
2022-10-21 15:06:49 +0200

    faster stepper

--- Stepper_Test.py
+++ Stepper_Test.py
@@ -10,11 +10,10 @@
 
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
-import time
-import board
+
 
 kit = MotorKit()
 
 for i in range(100):
-    kit.stepper1.onestep()
-    time.sleep(0.01)
+    kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+
