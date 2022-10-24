b7baa194d452b71f0f98740f4f92e4871ff15ad6
Lutz Hager <lutz.hager@online.de>
2022-10-24 22:03:30 +0200

    added example of Cobotta access

--- cobotta_Test.py
+++ cobotta_Test.py
@@ -5,6 +5,7 @@
 #   Date: 21.10.22
 #
 #----------------------------------------------------------------------------------------------------------------
+from Cobotta_Bauer import IHandl
 import pybcapclient.bcapclient as bcapclient
 import numpy as np
 # import cv2
@@ -23,3 +24,45 @@ print("Open Connection")
 # start b_cap Service
 m_bcapclient.service_start("")
 print("Send SERVICE_START packet")
+
+# set Parameter
+Name = ""
+Provider = "CaoProv.DENSO.VRC"
+Machine = "10.50.12.87"
+Option = ""
+
+# Connect to RC8 (RC8(VRC)provider)
+hCtrl = m_bcapclient.controller_connect(Name, Provider, Machine, Option)
+print("Connect RC8")
+
+#------------------------------------------------------------------------
+
+# get Object for reading a variable from Cobotta
+# "I1" = Identifier of the variable
+IHandl = 0
+IHandl = m_bcapclient.controller_getvariable(hCtrl, "I1", "")
+
+# read value of a variable from Cobotta
+# "I1" = Identifier of the variable
+retI = m_bcapclient.variable_getvalue(IHandl)
+
+#------------------------------------------------------------------------
+
+# put object for writing a variable zo Cobotta
+# "I1" = Identifier of the variable
+newVal = 15
+m_bcapclient.variable_putvalue(IHandl, newVal)
+
+#------------------------------------------------------------------------
+
+
+
+# example of how the robot access will be handled later
+I1_access = m_bcapclient.controller_getvariable(hCtrl, "I1", "")   # Object for variable access
+CobottaAccess = 1   # new value
+m_bcapclient.variable_putvalue(I1_access, CobottaAccess)
+
+
+P90_access = m_bcapclient.controller_getvariable(hCtrl, "P90", "")   # Object to post new Coordinates
+new_coords = [0,0,0] 
+m_bcapclient.variable_putvalue(P90_access, new_coords)    # write new coordinates
