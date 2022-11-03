1a252bd4d6e96ed1fff5a86bf6b637f818831ec9
Lutz Hager <lutz.hager@online.de>
2022-11-03 14:03:12 +0100

    testing image

--- Camera_Test.py
+++ Camera_Test.py
@@ -9,32 +9,33 @@ import cv2
 import logging
 
 
-capture = cv2.VideoCapture('http://admin:password@10.50.12.88/554')
-print(capture.isOpened())
-
-# class CAMERA:
-#     def __init__(self, camNum):
-#         try:
-#             self.cam = cv2.VideoCapture(camNum)
-#             print("started video")
-#         except:
-#             logging.warning("Error connection to Webcam")  # will print a message to the console
-
-#     def image(self):
-#         ret, frame = self.cam.read()
-#         #if image capture valid, then save
-#         if frame is None:
-#             logging.warning("Error capturing image")
-#         else:
-#             cv2.imwrite("TestImage.png", frame)
-
-#     def __del__(self):
-#         self.cam.release ()
+# capture = cv2.VideoCapture('http://admin:password@10.50.12.88/554')
+# print(capture.isOpened())
 
 
-# # cam1 = CAMERA(0)
+class CAMERA:
+    def __init__(self, camNum):
+        try:
+            self.cam = cv2.VideoCapture(camNum)
+            print("started video")
+        except:
+            logging.warning("Error connection to Webcam")  # will print a message to the console
+
+    def image(self):
+        ret, frame = self.cam.read()
+        #if image capture valid, then save
+        if frame is None:
+            logging.warning("Error capturing image")
+        else:
+            cv2.imwrite("TestImage.png", frame)
+
+    def __del__(self):
+        self.cam.release ()
+
+
 # cam1 = CAMERA(0)
-# cam1.image()
+cam1 = CAMERA(0)
+cam1.image()
 
 
 
