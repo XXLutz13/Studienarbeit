fixed coord calculation

Matplot_Test/Coords_Test.py
@@ -20,9 +20,9 @@ R = 50
 spacing = num_images//8
 
 phi = np.linspace(0, 0.5 * np.pi, spacing)
-X = []
-Y = Object_cords[1] + R * np.sin(phi)
-Z = Object_cords[2] + R * np.cos(phi)
+X = Object_cords[0] + R * np.cos(phi)
+Y = []
+Z = Object_cords[2] + R * np.sin(phi)
 dx = []
 dy = []
 dz = []
@@ -30,7 +30,7 @@ dz = []
 cords = []
 angle_x_increment = 90/spacing
 for x in range(spacing):
-    X += [Object_cords[1]]
+    Y += [Object_cords[1]]
     dx += [90 - x*angle_x_increment]
     dy += [0]
     dz += [90]