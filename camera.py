680280fda574a2e0b864cc59feef948c46897b71
XXLutz13 <lutz.hager@online.de>
2022-11-30 16:40:55 +0100

    testing motion

--- camera.py
+++ camera.py
@@ -2,31 +2,86 @@ import pybcapclient.bcapclient as bcapclient
 import cv2
 import numpy as np
 from datetime import datetime
+import time
 
+#----------------------------------------------------------------------------------------------------------------
+#   establish connection to Cobotta
+#   Input: IP_adress: IP
+#   Output: bcapclient: client
+#           RC8 controller: RC8 
+#----------------------------------------------------------------------------------------------------------------
+def connect_Cobotta(IP):
 
-# set IP Address , Port number and Timeout of connected RC8
-host = "10.50.12.87"
-port = 5007
-timeout = 2000
+    # set IP Address , Port number and Timeout of connected RC8
+    host = IP
+    port = 5007
+    timeout = 2000
 
-# Connection processing of tcp communication
-client = bcapclient.BCAPClient(host, port, timeout)
-print("Open Connection")
+    try:
+        # Connection processing of tcp communication
+        client = bcapclient.BCAPClient(host, port, timeout)
 
-# start b_cap Service
-client.service_start("")
-print("Send SERVICE_START packet")
+        # start b_cap Service
+        client.service_start("")
 
- # Get Camera Handler
-IP = '10.50.12.88'
-camera_handler = client.controller_connect('N10-W02', 'CaoProv.Canon.N10-W02', '', 'Conn=eth:'+ IP +', Timeout=3000')
-client = client
+        # set Parameter
+        Name = ""
+        Provider = "CaoProv.DENSO.VRC"
+        Machine = host
+        Option = ""
 
-for x in range(50):
-    # OneShot
-    begin = datetime.now()
-    client.controller_execute(camera_handler, 'OneShotFocus', '')
+        # Connect to RC8 (RC8(VRC)provider)
+        RC8 = client.controller_connect(Name, Provider, Machine, Option)
 
-    duration = datetime.now() - begin
-    print("Time: %s"% duration)
-    
+        print("Cobotta connected")
+        return client, RC8
+
+    except:
+        raise RuntimeError("can't connect to Cobotta")
+
+#----------------------------------------------------------------------------------------------------------------
+#   cobotta camera class
+#   Atributes: client = Cobotta connection
+#              IP = camera IP-adress
+#----------------------------------------------------------------------------------------------------------------
+class CAMERA:
+    def __init__(self, client, IP):
+        try:
+            # Get Camera Handler
+            self.camera_handler = client.controller_connect('N10-W02', 'CaoProv.Canon.N10-W02', '', 'Conn=eth:'+ IP +', Timeout=3000')
+            self.client = client
+        except:
+            raise RuntimeError("can't connect camera")
+
+    def OneShot(self, name):
+        try:
+            image = self.client.controller_execute(self.camera_handler, 'OneShotFocus', '')
+
+            # Get Variable ID
+            variable_handler = self.client.controller_getvariable(self.camera_handler, 'IMAGE')
+
+            image_buff = self.client.variable_getvalue(variable_handler)
+            # converts Cobotta image to usable numpy formate 
+            nparr = np.frombuffer(image_buff , dtype=np.uint8)
+            cv_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            # save image to file
+            image_name = 'Images/{}{}.png'
+            cv2.imwrite(image_name.format(datetime.now().strftime("%Y%m%d_%H:%M:%S"), name), cv_image)
+        except:
+            raise RuntimeError("faild to capture image")
+
+# converts Cobotta image to usable numpy formate 
+def convert_image(img):
+    nparry = np.frombuffer(img , dtype=np.uint8)
+    cv_image = cv2.imdecode(nparry, cv2.IMREAD_COLOR)
+    return cv_image
+
+# establish Cobotta connection
+client, RC8 = connect_Cobotta('10.50.12.87')
+# open camera connection
+CAM = CAMERA(client=client, IP='10.50.12.88')
+
+for x in range(20):
+    CAM.OneShot('_Test_')
+    time.sleep(1)
+    print(x)
